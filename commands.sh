e3e6f3463e323eda2916fc8008837f7e087a51ad
burhanyetis <burhanyetis51@hotmail.com>
2018-12-19 02:12:53 -0800

    commands

--- commands.sh
+++ commands.sh
@@ -1,1 +1,3 @@
+##############################COMMANDS#############################
+
 hadoop fs -ls hdfs://ma2-gbip-lnn11.corp.itt.com:8020/user/hive/gbi/edw/itunes/idaa/idaa_pipeline/paf_dqmstats_re_detail/ |sed 's/ */ /g'|sed -n '1!p'|awk -F "" '{print $8}'|awk -F "/" '{print $12}'|sort -r
