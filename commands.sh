2b86c5c375f9577f137bb7285da59495a9df940f
burhanyetis <burhanyetis51@hotmail.com>
2018-12-19 02:17:16 -0800

    commands

--- commands.sh
+++ commands.sh
@@ -1,6 +1,8 @@
 ##############################COMMANDS#############################
+1. hadoop fs -ls hdfs://ma2-gbip-lnn11.corp.itt.com:8020/user/hive/gbi/edw/itunes/idaa/idaa_pipeline/paf_dqmstats_re_detail/ |sed 's/ */ /g'|sed -n '1!p'|awk -F "" '{print $8}'|awk -F "/" '{print $12}'|sort -r
 
-hadoop fs -ls hdfs://ma2-gbip-lnn11.corp.itt.com:8020/user/hive/gbi/edw/itunes/idaa/idaa_pipeline/paf_dqmstats_re_detail/ |sed 's/ */ /g'|sed -n '1!p'|awk -F "" '{print $8}'|awk -F "/" '{print $12}'|sort -r
-
-
+2. part1 = "hdfs://TEST2/user/hive/dir1/dir2/"
+   part2 = "hdfs://TEST3/user/hive/dir1/dir2/"
+   hadoop distcp -Dmapred.job.queue.name = <queue_name> -skipcrccheck -pb -delete -update -m 20 part1 part2
+   
 
