75bce3d4d8a9f4fd8e78e20efcae9809a63203ca
burhanyetis <burhanyetis51@hotmail.com>
2019-04-15 21:27:14 -0700

    Update and rename commands.sh to hdfs data transfers.sh

--- hdfs data transfers.sh
+++ hdfs data transfers.sh
@@ -1,6 +1,8 @@
 ##############################COMMANDS#############################
+To get all the files on a hdfs directory:
 1. hadoop fs -ls hdfs://ma2-gbip-lnn11.corp.itt.com:8020/user/hive/gbi/edw/itunes/idaa/idaa_pipeline/paf_dqmstats_re_detail/ |sed 's/ */ /g'|sed -n '1!p'|awk -F "" '{print $8}'|awk -F "/" '{print $12}'|sort -r
 
+To move the one directory in hdfs to another direcotry in hdfs
 2. part1 = "hdfs://TEST2/user/hive/dir1/dir2/"
    part2 = "hdfs://TEST3/user/hive/dir1/dir2/"
    hadoop distcp -Dmapred.job.queue.name = <queue_name> -skipcrccheck -pb -delete -update -m 20 part1 part2
